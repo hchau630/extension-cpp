06a443135492a1998c01e6f9349aaa9de047cb53
Ho Yin Chau <hchau630@berkeley.edu>
2025-01-01 18:34:13 -0500

    trying to fix include error

--- extension_cpp/csrc/math.h
+++ extension_cpp/csrc/math.h
@@ -6,6 +6,6 @@ inline C10_HOST_DEVICE T calc_mymuladd(T a, T b, T c) {
 }
 
 template <typename T>
-inline C10_HOST_DEVICE T calc_mymul(T a, T b, T c) {
+inline C10_HOST_DEVICE T calc_mymul(T a, T b) {
   return a * b;
 }
